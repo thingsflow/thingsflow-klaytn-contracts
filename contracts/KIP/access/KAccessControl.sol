55f9e4531c023d67ffc6456566bef5c138076924
toniya-klaytn <toniya.sun@klaytn.foundation>
2022-06-09 11:24:23 +0800

    adding note

--- contracts/KIP/access/KAccessControl.sol
+++ contracts/KIP/access/KAccessControl.sol
@@ -2,6 +2,7 @@
 // Klaytn Contract Library v1.0.1 (KIP/access/KAccessControl.sol)
 // Inherited from Openzepplin Accesscontrol
 // https://github.com/OpenZeppelin/openzeppelin-contracts/releases/tag/v4.5.0
+// Note - adding KIP13 interface for a backward compatible implementation
 
 pragma solidity ^0.8.0;
 
